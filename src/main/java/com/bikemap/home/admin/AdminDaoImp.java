81d58b6ff70168ea71bf1a7b71facf4ac46936aa
BD <cnddn1114@gmail.com>
2020-11-19 16:46:15 +0900

    bd admin

---
+++ src/main/java/com/bikemap/home/admin/AdminDaoImp.java
@@ -0,0 +1,5 @@
+package com.bikemap.home.admin;
+
+public interface AdminDaoImp {
+	
+}
---
+++ src/main/webapp/WEB-INF/views/admin/AdminController.java
@@ -0,0 +1,26 @@
+package com.bikemap.home.admin;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+	SqlSession sqlSession;
+
+	public SqlSession getSqlSession() {
+		return sqlSession;
+	}
+	@Autowired
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+	
+	@RequestMapping("/adminHome")
+	public String adminUser() {
+		return "admin/adminHome";
+	}
+	
+	
+}
